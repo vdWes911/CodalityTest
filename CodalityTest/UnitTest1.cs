b4e440e3598c35c4bffe812b09d6375e51d42818
Stephan v.d. Westhuizen <stephanwest911@gmail.com>
2022-10-28 21:13:19 +0200

    Actual solution

--- CodalityTest/UnitTest1.cs
+++ CodalityTest/UnitTest1.cs
@@ -21,31 +21,17 @@ namespace CodalityTest
 
         public int solution(int X, int[] A)
         {
-            var frogPosition = 0;
-            var second = 0;
-            var expectedNextLeaf = frogPosition + 1;
-            var alreadyFallenPositions = new List<int>();
-
-            // Simulates seconds passing
-            for (second = 0; second < X + 2; second++)
+            bool[] has = new bool[X];
+            int count = 0;
+            for (var i = 0; i < A.Length; i++)
             {
-                var fallenLeaf = A[second];
-                alreadyFallenPositions.Add(fallenLeaf);
-
-                bool canJump = fallenLeaf == expectedNextLeaf || alreadyFallenPositions.Contains(expectedNextLeaf);
-                while (canJump)
+                if (has[A[i] - 1] == false)
                 {
-                    frogPosition++;
-                    expectedNextLeaf = frogPosition + 1;
-                    canJump = fallenLeaf == expectedNextLeaf || alreadyFallenPositions.Contains(expectedNextLeaf);
-                }
-
-                if (frogPosition == X)
-                {
-                    return second;
+                    has[A[i] - 1] = true;
+                    if (++count == X)
+                        return i;
                 }
             }
-
             return -1;
         }
 
