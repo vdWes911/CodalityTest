cf25a868417be5cc84c8c9b99a12c3c94219f62c
Stephan v.d. Westhuizen <stephanwest911@gmail.com>
2022-10-29 17:29:36 +0200

    WIP

--- CodalityTest/UnitTest1.cs
+++ CodalityTest/UnitTest1.cs
@@ -1,13 +1,41 @@
+using System.Linq;
 using NUnit.Framework;
 
 namespace CodalityTest
 {
-    public class Tests
+    public class Solution
     {
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var input = new int[] { 2, 1, 3 };
+            var s = 2;
+            var solAns = solution(input, s);
+            var ans = 3;
+
+            Assert.That(solAns, Is.EqualTo(ans));
+        }
+
+        public int solution(int[] A, int S)
+        {
+            var totalNumberOfSolutions = 0;
+
+            for (int i = 1; i <= A.Length; i++)
+            {
+                var tempArray = new int[i];
+
+                for (int j = 0; j < tempArray.Length; j++)
+                {
+                    tempArray[j] = A[j];
+                    var average = tempArray.Average();
+                    if (average == (double)S)
+                    {
+                        totalNumberOfSolutions++;
+                    }
+                }
+            }
+
+            return totalNumberOfSolutions;
         }
 
     }
